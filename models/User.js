4afb78396918b5d8c949a3485b70bb86934b73f7
TJ James <jamestw13@gmail.com>
2022-01-07 13:06:11 -0600

    initial setup of server and models

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const {Schema, model} = require('mongoose');
+const {isEmail} = require('validator');
+// TODO date format?
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: [isEmail, 'invalid email address'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [UserSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
